3405fde84cdca7d2c0c9da2e7654781a9633db3f
Claire Sosset <claire.sosset@gmail.com>
2015-03-19 10:05:37 +0100

    removing unnecessary import

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,6 @@ import Router from 'react-router';
 
 import routes from './routes/';
 
-// import Dispatcher from './dispatcher/';
-
 //Start app
 Router.run(routes, function (Handler) {
   React.render(<Handler/>, document.body);
